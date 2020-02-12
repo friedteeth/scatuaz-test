6df21a9ea29964936718ade1cf524c169004e067
Salvador <loeraquirozs@gmail.com>
2020-02-12 08:20:26 -0600

    Configured travis.yml

--- scatuaz/settings.py
+++ scatuaz/settings.py
@@ -78,13 +78,17 @@ WSGI_APPLICATION = 'scatuaz.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'scatuaz',
+    #     'USER': 'tigrito',
+    #     'PASSWORD': 'tigrito123@',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': 3306
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'scatuaz',
-        'USER': 'tigrito',
-        'PASSWORD': 'tigrito123@',
-        'HOST': '127.0.0.1',
-        'PORT': 3306
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
